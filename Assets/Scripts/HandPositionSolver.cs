412ab55349a4c0c6a296742f68cc1b0c15bbd9c1
Martha Miracky <mmiracky@umich.edu>
2022-11-20 11:51:03 -0500

    Initial Files

---
+++ Assets/Scripts/HandPositionSolver.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ARDK.Extensions;
+using Niantic.ARDK.AR.Awareness;
+
+public class HandPositionSolver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private ARHandTrackingManager handTrackingManager;
+    [SerializeField] private Camera arCamera;
+    [SerializeField] private float minHandConf = 0.85f;
+
+    private Vector3 handPosition;
+    public Vector3 HandPosition
+    {
+        get => handPosition;
+    }
+    void Start()
+    {
+        handTrackingManager.HandTrackingUpdated += HandTrackingUpdated;
+    }
+
+    private void OnDestroy()
+    {
+        handTrackingManager.HandTrackingUpdated -= HandTrackingUpdated;
+    }
+
+    private void HandTrackingUpdated(HumanTrackingArgs handData)
+    {
+        var detections = handData.TrackingData?.AlignedDetections;
+        if (detections == null)
+        {
+            return;
+        }
+
+        foreach (var detection in detections)
+        {
+            if (detection.Confidence < minHandConf)
+            {
+                return;
+            }
+
+            Vector3 detectionSize = new Vector3(detection.Rect.width, detection.Rect.height, 0);
+            float depthEstimation = 0.2f + Math.Abs(1 - detectionSize.magnitude);
+
+            handPosition = arCamera.ViewportToWorldPoint(new Vector3(detection.Rect.center.x,
+                1 - detection.Rect.center.y, depthEstimation));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlaceARObjectOnHand.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceARObjectOnHand : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private HandPositionSolver handPositionSolver;
+    [SerializeField] private GameObject arObject;
+    [SerializeField] private float speedMovement = 0.5f;
+    [SerializeField] private float speedRotation = 25.0f;
+
+    private float MiNDistance = 0.05f;
+    private float minAngleMagnitude = 2.0f;
+    private bool shouldAdjustRotation;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlaceObjectOnHand(handPositionSolver.HandPosition);
+    }
+
+    private void PlaceObjectOnHand(Vector3 handPosition)
+    {
+        float distance = Vector3.Distance(handPosition, arObject.transform.position);
+        arObject.transform.position =
+            Vector3.MoveTowards(arObject.transform.position, handPosition, speedMovement * Time.deltaTime);
+        if (distance >= MiNDistance)
+        {
+            arObject.transform.LookAt(handPosition);
+            shouldAdjustRotation = true;
+        }
+        else
+        {
+            if (shouldAdjustRotation)
+            {
+                arObject.transform.rotation =
+                    Quaternion.Slerp(arObject.transform.rotation, Quaternion.identity, 2 * Time.deltaTime);
+                Vector3 angles = arObject.transform.rotation.eulerAngles;
+                shouldAdjustRotation = angles.magnitude >= minAngleMagnitude;
+            }
+            else
+            {
+                arObject.transform.Rotate(Vector3.up * speedRotation * Time.deltaTime);
+            }
+        }
+    }
+    
+}
