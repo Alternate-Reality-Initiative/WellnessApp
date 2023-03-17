6bb1a1faf70827567f25c1edd8c612bc1a15817f
Celina Sarona <celin@0587458943.wireless.umich.net>
2023-03-17 19:12:25 -0400

    date time prefabs

---
+++ Assets/ChecklistTeamScenes/celina/week.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Globalization;
+
+
+public class week : MonoBehaviour
+{   
+    
+    DateTime date = System.DateTime.Now;
+    Calendar day;
+    DayOfWeek val;
+    public bool newWeek;
+
+    //detects new week that starts Sunday
+    void Awake(){
+        mesh_renderer = gameObject.GetComponent<MeshRenderer>();
+
+        day = CultureInfo.InvariantCulture.Calendar;
+        val = day.GetDayOfWeek(date);
+        
+
+        if (val != DayOfWeek.Sunday) {
+            newWeek = true; 
+                 
+        }
+
+
+        
+
+    }
+}
