ea17972d7866d2f95598bbaa5f8c366990af969e
Bella Palumbi <christabellapalumbi@gmail.com>
2023-03-25 22:43:47 -0400

    BP - Connect data to collection scene

--- Assets/ChecklistTeamScenes/AssetsforChecklist/hideAndShow.cs
+++ Assets/ChecklistTeamScenes/AssetsforChecklist/hideAndShow.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class hideAndShow : MonoBehaviour
 {
-  public GameObject the_back; 
+  public GameObject plantParent;
+  private GameObject currentActiveBg;
+  private Player player;
 
-  public void hide() {
-    the_back.SetActive(false);
+  void Start() {
+    player = GameObject.Find("Player").GetComponent<Player>();
+
+    // sets selected plant to the saved plant from the save data
+    GameObject bgOfAlreadySelected = plantParent.transform.GetChild(player.selectedPlant).Find("YellowBackground").gameObject;
+    ShowThis(bgOfAlreadySelected, player.selectedPlant);
   }
-  public void show(){
-    the_back.SetActive(true);
+
+  public void ShowThis(GameObject plantBg, int index) {
+   
+    if (currentActiveBg == null) {
+      // selecting a plant when none is selected
+      currentActiveBg = plantBg;
+      currentActiveBg.SetActive(true);
+    }
+    else if (plantBg != currentActiveBg) {
+      // selecting one when there is already one selected and it's different than the one just clicked
+      currentActiveBg.SetActive(false); 
+      currentActiveBg = plantBg;
+      currentActiveBg.SetActive(true);
+    }
+
+    // Important! This changes the selected plant index in data
+    player.selectedPlant = index;
   }
 }
