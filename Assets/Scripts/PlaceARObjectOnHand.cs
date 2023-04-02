a79c4ad2e5d886ebd019a5b73292d5f53c37249d
Derek Yang <dereky@umich.edu>
2023-04-02 13:28:30 -0400

    Connect plant data with health display. Add functionality for reviving plant and cannot water with negative water. Plant health only increases when water collides with plant.

--- Assets/Scripts/PlaceARObjectOnHand.cs
+++ Assets/Scripts/PlaceARObjectOnHand.cs
@@ -29,12 +29,19 @@ public class PlaceARObjectOnHand : MonoBehaviour
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
         _plantIndex = _player.selectedPlant;
-        //currentHealth = _plantHealth[_plantIndex];
+        currentHealth = _plantHealth[_plantIndex];
         plantArr = new List<PlantScriptableObject>();
         plantArr = Resources.LoadAll<PlantScriptableObject>("Plant SO").Cast<PlantScriptableObject>().ToList();
         plant = plantArr[_plantIndex];
-        GameObject plantModel = plant.plantObject;
-        arObject = Instantiate(plantModel, new Vector3(0,0,5), Quaternion.Euler(new Vector3(-90,0,0)));
+        if (currentHealth == 0)
+        {
+            arObject = Instantiate(emptyPot, new Vector3(0, 0, 5), Quaternion.Euler(new Vector3(-90, 0, 0)));
+        }
+        else
+        {
+            GameObject plantModel = plant.plantObject;
+            arObject = Instantiate(plantModel, new Vector3(0, 0, 5), Quaternion.Euler(new Vector3(-90, 0, 0)));
+        }
         arObject.transform.localScale = new Vector3(10,10,10); 
     }
 
