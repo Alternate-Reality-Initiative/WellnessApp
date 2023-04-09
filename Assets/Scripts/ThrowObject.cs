e90375e386cd1d756bccb51c485cc11b1b33f324
Derek Yang <dereky@umich.edu>
2023-04-09 14:18:54 -0400

    Add revive UI and debug testing for hearts

--- Assets/Scripts/ThrowObject.cs
+++ Assets/Scripts/ThrowObject.cs
@@ -1,7 +1,10 @@
 //Standard Unity/C# functionality
 using UnityEngine;
 using System;
+using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 //These tell our project to use pieces from the Lightship ARDK
 using Niantic.ARDK.AR;
@@ -22,13 +25,16 @@ public class ThrowObject : MonoBehaviour
     public Camera _mainCamera;  //This will reference the MainCamera in the scene, so the ARDK can leverage the device camera
     IARSession _ARsession;  //An ARDK ARSession is the main piece that manages the AR experience
     public TMP_Text _waterCounter; //Need to get the amount of water from saved data
-    public int waterCount;
     public Player _player;
     public int[] plantHealths;
     public int plantIndex;
     public int currentHealth;
-    public Collision collision;
+    //public Collision collision;
     public GameObject plant;
+    public GameObject promptPanel;
+    public Button spendWaterButton;
+    public TextMeshProUGUI promptWaterCost;
+    private List<PlantScriptableObject> plantArr;
 
     // Start is called before the first frame update
     void Start()
@@ -42,7 +48,9 @@ public class ThrowObject : MonoBehaviour
         currentHealth = plantHealths[plantIndex];
         PlaceARObjectOnHand script = GetComponent<PlaceARObjectOnHand>();
         plant = script.arObject;
-        collision = plant.GetComponent<Collision>();
+        //collision = plant.GetComponent<Collision>();
+        plantArr = new List<PlantScriptableObject>();
+        plantArr = Resources.LoadAll<PlantScriptableObject>("Plant SO").Cast<PlantScriptableObject>().ToList();
 
 
         //_player.waterLevel = int.Parse(_waterCounter.text);
@@ -63,7 +71,7 @@ public class ThrowObject : MonoBehaviour
     //This function will be called when the player touches the screen. For us, we'll have this trigger the shooting of our object from where we touch.
     public void ClickButton()
     {
-        if (_player.waterLevel > 0 && plantHealths[plantIndex] > 0) {
+        if (_player.waterLevel > 0 && plantHealths[plantIndex] > 0) { // water button
             System.Random rnd = new System.Random();
             var radius = .2;
 
@@ -78,7 +86,7 @@ public class ThrowObject : MonoBehaviour
                 rigbod.velocity = new Vector3(0f, 0f, 0f);
                 float force = 300.0f;
                 rigbod.AddForce(_mainCamera.transform.forward * force);
-                StartCoroutine(Evaporate(newObject));
+                //StartCoroutine(Evaporate(newObject));
             }
 
             _player.waterLevel--; // decrement the player's water level
@@ -86,29 +94,10 @@ public class ThrowObject : MonoBehaviour
 
             //Debug.Log(_player.waterLevel);
         }
-        else if (_player.waterLevel >= 5 && plantHealths[plantIndex] == 0)
+        else if (plantHealths[plantIndex] == 0) // revive button
         {
-            System.Random rnd = new System.Random();
-            var radius = .2;
-
-            for (int i = 0; i < 15; i++)
-            {
-                double angle = rnd.NextDouble() * (2 * Math.PI);
-
-                GameObject newObject = Instantiate(_objectPrefab);  //Spawn a new object from our Ball Prefab
-                newObject.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));   //Set the rotation of our new object
-                newObject.transform.position = _mainCamera.transform.position + _mainCamera.transform.forward;    //Set the position of our new object to just in front of our Main Camera
-                newObject.transform.Translate((float)(radius * Math.Cos(angle)), (float)(radius * Math.Sin(angle)), 0);
-                Rigidbody rigbod = newObject.GetComponent<Rigidbody>();
-                rigbod.velocity = new Vector3(0f, 0f, 0f);
-                float force = 300.0f;
-                rigbod.AddForce(_mainCamera.transform.forward * force);
-                StartCoroutine(Evaporate(newObject));
-            }
-
-            for (int i = 0; i < 5; i++) _player.waterLevel--; // decrement water level 5 times
-            _waterCounter.text = _player.waterLevel.ToString();
-            _player.plantHealth[plantIndex] = 3; // revive the selected plant's health back to 3
+            ShowPrompt();
+            SpendWaterToReviveOnClick();
         }
         //else if (_player.waterLevel == 0)             // FOR TESTING
         //{
@@ -116,16 +105,61 @@ public class ThrowObject : MonoBehaviour
         //    _waterCounter.text = _player.waterLevel.ToString();
         //}
     }
-    IEnumerator Evaporate(GameObject droplet){
-        if (collision.gameObject.tag == "raindrop" && _player.plantHealth[_player.selectedPlant] < 3)
+    //IEnumerator Evaporate(GameObject droplet){
+    //    if (collision.gameObject.tag == "raindrop" && _player.plantHealth[_player.selectedPlant] < 3)
+    //    {
+    //        _player.plantHealth[_player.selectedPlant]++;
+    //        Destroy(droplet);
+    //    }
+    //    else
+    //    {
+    //        yield return new WaitForSeconds(2f);
+    //        Destroy(droplet);
+    //    }
+    //}
+    private void ShowPrompt()
+    {
+        promptPanel.SetActive(true);
+
+        PlantScriptableObject plantInfo = plantArr[plantIndex];
+        TextMeshProUGUI spendButtonText = spendWaterButton.gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        if (_player.waterLevel < 5)
         {
-            _player.plantHealth[_player.selectedPlant]++;
-            Destroy(droplet);
+            spendButtonText.text = "Not Enough Water";
+            spendButtonText.fontSize = 50;
         }
         else
         {
-            yield return new WaitForSeconds(2f);
-            Destroy(droplet);
+            spendWaterButton.interactable = true;
+            spendButtonText.text = "YES";
+            spendButtonText.fontSize = 60;
+            promptWaterCost.text = "Spend 5 Water\n to Revive \n" + plantInfo.name + "?";
+        }
+    }
+    public void HidePrompt()
+    {
+        promptPanel.SetActive(false);
+    }
+
+    private void SpendWaterToReviveOnClick()
+    {
+        if (_player.waterLevel >= 5)
+        {
+            _player.waterLevel -= 5;
+            _waterCounter.text = _player.waterLevel.ToString();
+            _player.plantHealth[plantIndex] = 3; // revive the selected plant's health back to 3
+
+            // hide this menu
+            HidePrompt();
+
+            // set model back from empty pot to normal plant
+            GameObject plantModel = plantArr[plantIndex].plantObject;
+            plant = Instantiate(plantModel, new Vector3(0, 0, 5), Quaternion.Euler(new Vector3(-90, 0, 0)));
+            plant.transform.localScale = new Vector3(10, 10, 10);
+            if (plantArr[plantIndex].name == "Sprout")
+            {
+                plant.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            }
         }
     }
 }
