8b730c3a4a926f0a40f448f7a1022c66ff99c9e1
Bella Palumbi <christabellapalumbi@gmail.com>
2023-04-14 17:58:28 -0400

    Fix bug where unlocked plant data wasn't copied over correctly

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -75,18 +75,7 @@ public class Player : MonoBehaviour
         }
 
 
-        // unlockedPlants = data.unlockedPlants;
-        // int unlockSum = 0;
-        // for (int i = 0; i < NUM_PLANTS; i++) {
-        //     if (unlockedPlants[i] == true) {
-        //         unlockSum++;
-        //     }
-        // }
-        // if (unlockSum == 0) {
-        //     unlockedPlants = new bool[NUM_PLANTS];
-        //     unlockedPlants[SPROUT_INDEX] = true;
-        //     selectedPlant = SPROUT_INDEX;
-        // }
+        unlockedPlants = data.unlockedPlants;
 
         previous = data.previous;
         Debug.Log("prev initial" + previous);
